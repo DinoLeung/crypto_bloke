612c1bc50b3fb094719035627afa4168811c6cb5
dino <dino@330z.net>
2018-02-11 23:12:55 +1100

    Displaying some of the data
    High cpu usage, need to find a better way

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
 const {remote, ipcRenderer} = require('electron')
 var CoinCap = remote.require('./coinCap')
 
-var testBtn = document.getElementById('testBtn')
-var testText = document.getElementById('testText')
+// var testBtn = document.getElementById('testBtn')
+// var testText = document.getElementById('testText')
 
-testBtn.addEventListener('click', function (event) {
-})
+// testBtn.addEventListener('click', function (event) {
+// })
 
-ipcRenderer.on('ready', (event, index) => {
-  testText.innerText = JSON.stringify(CoinCap.coinData[0])
+ipcRenderer.on('ready', (event) => {
+  try {
+    var html = '<table style="width:100%">'
+    CoinCap.coinData.forEach(item => {
+      html += '<tr id="' + item.short + '">'
+      html += '<th>' + item.long + '</th>'
+      html += '<th>' + item.price + '</th>'
+      html += '</tr>'
+    })
+    // testText.innerText = CoinCap.coinData[0].long + ': ' + CoinCap.coinData[0].price
+    html += '</table>'
+    document.getElementById('coinTable').innerHTML = html
+  } catch (error) {
+    // do nothing
+  }
 })
 
 ipcRenderer.on('update', (event, index) => {
-  // show btc only atm
-  testText.innerText = JSON.stringify(CoinCap.coinData[0])
-  // testText.innerText = JSON.stringify(CoinCap.coinData[index])
+  try {
+    // testText.innerText = CoinCap.coinData[0].long + ': ' + CoinCap.coinData[0].price
+    let th = document.getElementById(CoinCap.coinData[index].short)
+    th.innerHTML = '<th>' + CoinCap.coinData[index].long + '</th><th>' + CoinCap.coinData[index].price + '</th>'
+  } catch (error) {
+    // do nothing
+  }
 })
